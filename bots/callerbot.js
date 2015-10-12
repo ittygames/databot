62b2120b81b8722a51ecbb30f6c3c758028c8c96
ittytheitstar <Carl.Wright@cpit.ac.nz>
2015-10-12 22:11:19 +1300

    Update with broken caller bot https issue

--- bots/callerbot.js
+++ bots/callerbot.js
@@ -1,14 +1,103 @@
+var https = require('https');
 
 
-//every bot must have this method, it must return a string or null;
-//the string will be returned to the user in a message
- class CallerBot {
-
-    static function doAction($slack, $slackData, $bot, $command1, $command2, $botData)
-
+exports.doAction = function($slack, $slackData, $bot, $command1, $command2, $botData)
     {
-        console.log("I made it to here");
+    // correctly align the json data to first in array
+    $botData = $botData[0];
+
+        try {
+            $data = $botData.data;
+            $method = $botData.method;
+            $host = $botData.host;
+
+            if($host.charAt(0) == "<"){
+                $host = $host.substring(1);
+            }
+
+            if($host.charAt($host.length -1) == ">"){
+                $host = $host.substring(0, $host.length - 1);
+            }
+
+        } catch (e) {
+            return null;
+        }
+
+        try {
+            $headers = $botData.headers;
+        } catch (e) {
+            $headers ={
+                'Content-Type' : 'application/json',
+                'Content-Length' : JSON.stringify($data).length
+            };
+        }
+
+        try {
+            $port = $botdata.port;
+        } catch (e) {
+            $port = 443;
+        }
+
+        try {
+            $path = $botdata.path;
+        } catch (e) {
+            $path = "";
+        }
+
+        try {
+            $dumpHeaders = $botData.dumpHeaders;
+        } catch (e) {
+            $dumpHeaders = false;
+        }
+        try {
+            $dumpResponse = $botData.dumpResponse;
+        } catch (e) {
+            $dumpResponse = true;
+        }
+
+        switch($method){
+
+            case "POST":
+                var optionspost = {
+                    host : $host,
+                    method : $method,
+                    headers : $headers
+                };
+
+                var reqPost = https.request(optionspost, function(res) {
+
+                    if(res.statusCode == 200){
+                        console.log('looks like the request was a success');
+                    }else{
+                        console.log('looks like the request wasnt a success');
+                        console.log("statusCode: ", res.statusCode);
+                    }
+
+                    if($dumpHeaders){
+                        console.log("headers: ", res.headers)
+                    }
+
+                    res.on('data', function(dataReturned) {
+                        console.log(dataReturned);
+                    });
+                });
+
+                break;
+
+            case "GET":
+
+
+                break;
+
+            default:
+
+
+                break;
+        }
+
     }
 
-}
+
+
+
 
